76851d13422264692d0a1bb3687d714c7b22571c
Anita Growsoon <anitagrowsoon@gmail.com>
2015-10-11 02:37:10 -0400

    started hw

---
+++ app.js
@@ -0,0 +1,30 @@
+var turnCount = 0;
+
+
+
+var player = function getPlayer(){
+   if((turnCount % 2) === 0) {
+      return playerOne;
+   } else {
+      return playerTwo;
+   }
+}
+
+var Cell = function {
+   this.visibility = ;
+   this.value = ;
+}
+
+var board = {
+   row1: ['', '', ''],
+   row2: ['', '', ''],
+   row3: ['', '', '']
+}
+
+var game = {
+
+
+
+
+   turnCount+=1;
+};
