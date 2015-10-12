04836724674cc84db2926e510453c94f278494e2
Anita Growsoon <anitagrowsoon@gmail.com>
2015-10-12 16:49:31 -0400

    working on check for winner. almost done!

--- app.js
+++ app.js
@@ -1,108 +1,145 @@
-// var player = function(){
-//    if((turnCount % 2) === 1) {
-//       return playerOne;
-//    } else {
-//       return playerTwo(value) {
-//          this.value = value;
-//          this.wins = '';
-//       };
-//    }
-// };
+turnCount = 0;
 
-function Cell() {
-   // //new cells should be divs with class "cell"
-   // //this = document.createElement('div');
-   // this.className = 'cell';
-   //
-   // //value should be X or O if played
-   // //nothing if not played yet.
-   // this.value = '';
-   //
-   // //indicates whether cell has been played
-   // this.playable = 'true';
-   //
-   // //changes play state of cell and sets value
-   // //value is set based on player
-   // this.toggle = function(player){
-   //    if (player === playerOne){
-   //       this.value = 'X';
-   //    } else {
-   //       this.value = 'O';
-   //    }
-   //    this.removeEventListener();
-   // };
-   // this.addEventListener('click', function(event){
-   //    select = 'true';
-   //    cellSelected.push(this);
-   //    toggle();
-   // }bind(this););
-   //    }
-   // }
+function player(){
+   if((turnCount % 2) === 1) {
+      return '<img src="http://www.trinitybc.org/vbs2010/files/horseshoe%20home.png" id="horseshoe"></img>';
+   } else {
+      return '<img src="http://themixnow.fm/wp-content/uploads/2013/03/Badge.png" id="sherrif"></img>';
+   }
+};
 
+function Cell(value) {
+   this.value = value;
+   this.id = id;
+   this.setState = function(input){
+      this.value = input;
+   };
+   this.playable = true;
+   this.addEventListener('click', function(){
+      if (playable)
+         var data = player();
+         this.innerHTML = this.data;
+   });
 }
 
-function Board(){
-   // this.addEventListener('click', function drawBoard(){
-   //    ????
-   // })
-   // r1: ['', '', ''],
-   // r2: ['', '', ''],
-   // r3: ['', '', ''],
-   // c1: [this.r1[0], this.r2[0], this.r3[0]],
-   // c2: [this.r1[1], this.r2[1], this.r3[1]],
-   // c3: [this.r1[2], this.r2[2], this.r3[2]],
-   // getWinner();
-   // gameOver();
-}
+var drawBoard = function(){
+   var row = '';
+   var column = '';
+   var idNum = 1;
+   var boardDiv = document.getElementById('game');
+   boardDiv.innerHTML = '';
+   for (var r=1; r<4; r++){
+      console.log('row is ' +r);
+      row = document.createElement('ul');
+      row.className = 'row';
+      row.id = 'r'+r;
+      for (var c=1; c<4; c++){
+         console.log('column is '+c);
+         column = document.createElement('li');
+         column.setAttribute('value', ' ');
+         column.setAttribute('playable', true);
+         column.addEventListener('click', makePlay);
+         column.className = 'c'+c;
+         column.id = idNum;
+         column.innerHTML = ' ';
+         idNum++;
+         row.appendChild(column);
+      } boardDiv.appendChild(row);
+   }
+};
 
-// getWinner = function(board){
+function makePlay(){
+   if (this.getAttribute('playable')){
+   this.setAttribute('value', player());
+   this.innerHTML = player();
+   turnCount++;
+   this.setAttribute('playable', false);
+   } else {
+      alert('that tile has already been chosen');
+   }
+   checkForMatch = function(){
+      console.log('check for match');
+   }
+   if (turnCount>9){
+      // console.log(this);
+      console.log('Game over!');
+      game.gameOn=false;
+      var reset = document.createElement('button');
+      reset.innerHTML = 'Click to try again, punk';
+      document.getElementById('info').appendChild(reset);
+      reset.addEventListener('click', game.start);
+   }
+};
+
+function getWinner(){
+   var chkMe = []
+   for (var i=1; i<4; i++){
+      chkMe.push(document.querySelectorAll('c'+i).getAttribute('innerHTML');
+      if (chkMe[i] === chkMe)
+   }
+};
+
 //    //if match across row
-//    if ((board.r1[0] === board.r1[1] && board.r1[1] === board.r1[2]) ||
-//       (board.r2[0] === board.r2[1] && board.r2[1] === board.r2[2]) ||
-//       (board.r3[0] === board.r3[1] && board.r3[1] === board.r3[2])) {
+//    if ((board[0][0] === board[0][1] && board[0][1] === board[0][2]) ||
+//       (board[1][0] === board[1][1] && board[1][1] === board[1][2]) ||
+//       (board[2][0] === board[2][1] && board[2][1] === board[2][2])) {
 //          this.gameOver(player);
 //    }
 //    //match down column
-//    else if ((board.c1[0] === board.c1[1] && board.c1[1] === board.c1[2]) ||
-//       (board.c2[0] === board.c2[1] && board.c2[1] === board.r2[2]) ||
-//       (board.c3[0] === board.c3[1] && board.c3[1] === board.r3[2])) {
+//    else if ((board[0][0] === board[1][0] && board[1][0] === board[2][0]) ||
+//       (board[0][1] === board[1][1] && board[1][1] === board[2][1]) ||
+//       (board[0][2] === board[1][2] && board[1][2] === board[2][2])) {
 //          this.gameOver(player);
 //    }
 //    //diagonal match
-//    else if ((board.r1[0] === board.r2[1] && board.r2[1] === board.r3[2]) ||
-//    (board.r1[2] === board.r2[1] && board.r2[1] === board.r3[0])) {
+//    else if ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+//    (board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
 //          this.gameOver(player);
-//    } else (())
+//    } else {
 //    gameOver = function(){
 //       gameOn = 'false';
 //       return player() + ' is the winner!';
 //    }
+// };
 
-var game = function(){
+var game = {
    // addEventListener('click')
-   var gameOn = false;
-   var turnCount = 1;
-   var board = makeBoard();
-   function start(){
-      gameOn = true;
-      //board.render();
-   }
-   function getMove(){
-      console.log("what's your move?")
-   }
-
-   function makeBoard(){
-      return new Board();
-   }
-
-   while (gameOn){
-      console.log(turnCount)
-      turnCount++;
-      if (turnCount>9){
-         console.log('Game over!');
-         gameOn=false;
+   gameOn: false,
+   maxTurns: 9,
+   start: function(){
+      game.gameOn = true;
+      turnCount = 1,
+      document.getElementById('info').innerHTML = '';
+      if (document.getElementById('start')){
+         document.querySelector('.button').removeChild(button);
       }
-   }
+      drawBoard();
+   },
 };
 
-game();
+
+var button = document.getElementById('start');
+button.addEventListener('click', game.start);
+
+
+// var game = {
+//    // addEventListener('click')
+//    gameOn: false,
+//    turnCount: 1,
+//    maxTurns: 9,
+//    start: function(){
+//       document.getElementById('info').innerHTML = '';
+//       this.gameOn = true;
+//       drawBoard();
+//       while (this.gameOn){
+//          // console.log(this);
+//          console.log('turnCount:' + this.turnCount)
+//          this.turnCount++;
+//          if (this.turnCount>this.maxTurns){
+//             // console.log(this);
+//             console.log('Game over!');
+//             this.gameOn=false;
+//          }
+//       }
+//    },
+// };
